50ee88700d43d0e2b14d926b1138a1b66fc2e427
Antoine <45072872+SlayerOfShadow@users.noreply.github.com>
2023-11-28 17:56:43 +0100

    islands and terrain

--- Assets/Scripts/BridgeTrigger.cs
+++ Assets/Scripts/BridgeTrigger.cs
@@ -4,13 +4,17 @@ public class BridgeTrigger : MonoBehaviour
 {
     [SerializeField] GameOfLife bridge;
     [SerializeField] string creatureTag;
+    [SerializeField] int buildMultiplier;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == creatureTag)
         {
-            bridge.Build();
-            print("build");
+            for (int i = 0; i < buildMultiplier; i++)
+            {
+                bridge.Build();
+                print("build");
+            }
         }
     }
 }
