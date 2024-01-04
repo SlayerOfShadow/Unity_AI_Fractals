e7b6eae3f1f3be6a832e63c70110cce494ca842b
Antoine <45072872+SlayerOfShadow@users.noreply.github.com>
2024-01-05 00:38:06 +0100

    change bridge direction

--- Assets/Scripts/Bridge/TrueBridge.cs
+++ Assets/Scripts/Bridge/TrueBridge.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class TrueBridge : MonoBehaviour
 {
+    public Vector3 direction;
     public GameObject cubePrefab;
     public int iterations = 0;
 
     public void Build()
     {
-        Instantiate(cubePrefab, new Vector3(transform.position.x - (0.5f * iterations), transform.position.y, transform.position.z), Quaternion.identity, transform);
+        if (direction.x != 0) cubePrefab.transform.localScale = new Vector3(0.5f, 0.5f, 2f);
+        else if (direction.z != 0) cubePrefab.transform.localScale = new Vector3(2f, 0.5f, 0.5f);
+        Instantiate(cubePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z) - (0.5f * iterations) * direction, Quaternion.identity, transform);
         iterations += 1;
     }
 }
