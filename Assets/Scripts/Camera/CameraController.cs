701da3c449c857c9d0d2ed68aa650021e71cb4ed
Antoine <45072872+SlayerOfShadow@users.noreply.github.com>
2024-01-14 18:03:30 +0100

    camera islands positions

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -7,8 +7,11 @@ public class CameraController : MonoBehaviour
     [SerializeField] Transform target;
     Vector3 startPosition;
     Quaternion startRotation;
-    [SerializeField] float smoothSpeed = 0.125f;
+    [SerializeField] float smoothSpeed = 0.05f;
     [SerializeField] Vector3 offset;
+    bool onCreature = false;
+
+    public List<Transform> cameraPositions = new List<Transform>();
 
     void Start()
     {
@@ -28,24 +31,42 @@ public class CameraController : MonoBehaviour
                 if (hit.collider.CompareTag("creature"))
                 {
                     target = hit.collider.transform;
+                    onCreature = true;
                 }
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            target = null;
-            transform.position = startPosition;
-            transform.rotation = startRotation;
+            target = cameraPositions[0];
+            onCreature = false;
+        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            target = cameraPositions[1];
+            onCreature = false;
+        } else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            target = cameraPositions[2];
+            onCreature = false;
+        } else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            target = cameraPositions[3];
+            onCreature = false;
         }
 
         if (target)
         {
-            Vector3 desiredPosition = target.position + offset;
+            Vector3 desiredPosition = target.position;
+            if (onCreature) desiredPosition += offset;
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
             transform.position = smoothedPosition;
 
-            transform.LookAt(target);
+            if (onCreature) transform.LookAt(target);
+            else {
+                Quaternion desiredRotation = target.rotation;
+                Quaternion smoothedRotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
+                transform.rotation = smoothedRotation;
+            }
         }
     }
 }
