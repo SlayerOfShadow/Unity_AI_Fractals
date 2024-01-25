580f744cbb6fdaa236745624890b22beddc7f8f6
Antoine <45072872+SlayerOfShadow@users.noreply.github.com>
2024-01-25 11:25:35 +0100

    big tree sound

--- Assets/Scripts/Trees/BigTreeManager.cs
+++ Assets/Scripts/Trees/BigTreeManager.cs
@@ -6,11 +6,19 @@ public class BigTreeManager : MonoBehaviour
 {
     [SerializeField] GameObject[] bigTrees;
     public int bridgeCompleted = 0;
+    AudioSource audioData;
+    [SerializeField] AudioClip clip;
+
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
 
     public void UpdateBigTree()
     {
         bridgeCompleted += 1;
         bigTrees[bridgeCompleted - 1].SetActive(false);
         bigTrees[bridgeCompleted].SetActive(true);
+        audioData.PlayOneShot(clip);
     }
 }
